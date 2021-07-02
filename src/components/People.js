44de67721205fdffd3e4c9d06779ee812c24fe60
Abhishek Saxena <abhishek05101998@gmail.com>
2021-07-02 21:11:05 +0530

    pics reorganized

--- src/components/People.js
+++ src/components/People.js
@@ -49,31 +49,6 @@ function People() {
         <div className="col">
           <div className="card p-1 person" style={{ border: "none" }}>
             <img
-              src={process.env.PUBLIC_URL + "/adam.jpeg"}
-              className="card-img-top"
-              alt="..."
-            />
-            <div className="card-body">
-              <h5 className="card-title lead">Adam Fattal</h5>
-              <small className="card-text text-muted">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-bookmark"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z" />
-                </svg>{" "}
-                Undergrad Physics @ <em>Durham University</em>
-              </small>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="card p-1 person" style={{ border: "none" }}>
-            <img
               src={process.env.PUBLIC_URL + "/emilio.jpeg"}
               className="card-img-top"
               alt="..."
@@ -143,6 +118,32 @@ function People() {
                   <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z" />
                 </svg>{" "}
                 Undergrad CS @ <em>University of Chicago</em>
+              </small>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card p-1 person" style={{ border: "none" }}>
+            <img
+              src={process.env.PUBLIC_URL + "/adam.jpeg"}
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title lead">Adam Fattal</h5>
+              <small className="card-text text-muted">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-bookmark"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z" />
+                </svg>{" "}
+                Undergrad Physics @ <em>Durham University</em>
               </small>
             </div>
           </div>
